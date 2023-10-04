05bd4a769bef3ac03454852b307affda8a4e3d18
Thorben Ortmann <thorben.ortmann.dev@gmail.com>
2023-10-04 11:55:15 +0200

    dried code a bit more

--- fer/main.py
+++ fer/main.py
@@ -23,20 +23,17 @@ def ping() -> str:
 @app.post('/recognize/base64')
 async def recognize_emotions_from_base64_string(base64_image: str = Body(...)) -> Dict[str, float]:
     image_bytes = base64.b64decode(base64_image)
-    image_byte_array = np.frombuffer(image_bytes, np.uint8)
-
-    return recognize_emotions(image_byte_array)
+    return recognize_emotions(image_bytes)
 
 
 @app.post('/recognize/file')
 async def recognize_emotions_from_file(file: UploadFile = File(...)) -> Dict[str, float]:
     image_bytes = await file.read()
+    return recognize_emotions(image_bytes)
+
+
+def recognize_emotions(image_bytes: bytes) -> Dict[str, float]:
     image_byte_array = np.frombuffer(image_bytes, np.uint8)
-
-    return recognize_emotions(image_byte_array)
-
-
-def recognize_emotions(image_byte_array: np.ndarray) -> Dict[str, float]:
     bgr_image = cv2.imdecode(image_byte_array, cv2.IMREAD_COLOR)
     rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
 
